82fe93fc750728a2e3917a497b321c73fee859f2
Oussama Driouech <thedriwsh@gmail.com>
2026-06-24 04:15:10 +0100

    refactor(homepage): remove homepage search form from hero section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -85,51 +85,7 @@
   <!-- Header -->
   <header class="header" id="header">
   @include('components/navbar')
-    <div class="container-fluid search-form">
-            <form class="row g-3 align-items-center" action="{{ route('VillasPage') }}" method="GET">
-              @csrf
-                <div class="col-md-2 d-flex flex-column  align-items-center">
-                    <label for="checkin" class="form-label px-2 py-2 text-capitalize">{{__('messages.check-in')}}</label>
-                    <input type="date" name="date_checkin" class="form-control shadow-none bg-transparent border" id="checkin">
-                </div>
-                <div class="col-md-2 d-flex flex-column  align-items-center">
-                    <label for="checkout" class="form-label px-2 py-2 text-capitalize">{{__('messages.check-out')}}</label>
-                    <input type="date" name="date_checkout" class="form-control none-outline" id="checkout">
-                </div>
-                <div class="col-md-2 d-flex flex-column  align-items-center">
-                    <label for="type" class="form-label px-2 py-2 text-capitalize">{{__('messages.property_type')}}</label>
-                    <select name="property_type" id="type" class="form-select bg-transparent border w-100 none-outline py-1 px-3">
-                        <option selected>{{__('messages.select')}}</option>
-                        @foreach ($property_type as $type)
-                            <option value="{{$type}}">{{ __('messages.property_types.' . $type ) }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                 <div class="col-md-2 d-flex flex-column  align-items-center">
-                    <label for="type" class="form-label px-2 py-2 text-capitalize">{{__('messages.property_location')}}</label>
-                    <select name="property_location" id="type" class="form-select none-outline bg-transparent w-100 border py-1 px-3">
-                        <option selected>{{__('messages.select')}}</option>
-                         @foreach ($cities as $city)
-                            <option value="{{$city}}">{{ __('messages.cities.' . $city ) }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                  <div class="col-md-2 d-flex flex-column  align-items-center">
-                    <label for="type" class="form-label px-2 py-2 text-capitalize">{{__('messages.property_cost')}}</label>
-                    <select name="property_cost" id="type" class="form-select bg-transparent w-100 border py-1 px-3">
-                        <option selected>{{__('messages.select')}}</option>
-                            @foreach ($costs as $cost)
-                              <option value="{{$cost}}"><span>{{__('messages.greater_than')}}</span> {{$cost}} <span>MAD</span></option>
-                            @endforeach
-                    </select>
-                </div>
-                <div class="col-md-2 d-flex align-items-end m-auto">
-                    <button type="submit" class="primary_button btn shadow py-2  my-2  w-100 ">{{__('messages.filter_search')}}</button>
-                </div>
-            </form>
-
-        </div>
-    <div class="hero p-2">
+    <div class="hero">
       <div class="hero-slider">
         <div class="hero-slide active" style="background-image: url('{{ asset('images/bg/heroBg.png') }}')">
           <div class="hero-slide-content">
